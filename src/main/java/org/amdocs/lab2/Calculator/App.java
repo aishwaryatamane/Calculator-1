6ff97cbc82994e6576bf3e6620bfa1b0b17b7c74
suresh <scs.6190@gmail.com>
2019-07-10 11:42:22 +0530

    maven quick start project

---
+++ src/main/java/org/amdocs/lab2/Calculator/App.java
@@ -0,0 +1,13 @@
+package org.amdocs.lab2.Calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
