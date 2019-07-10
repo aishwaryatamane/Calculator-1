d28f60ccbf7831a57dad7a104406facadb8eca5d
shubham <s.a.dadhich@gmail.com>
2019-07-10 15:44:19 +0530

    App.java for main class

--- src/main/java/org/amdocs/lab2/Calculator/App.java
+++ src/main/java/org/amdocs/lab2/Calculator/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+	Substraction substraction = new Substraction();
+	substraction.evaluate(3,2);
     }
 }
